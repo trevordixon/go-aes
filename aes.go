004c33fd527ed30243211d2d63a3e5f8d7b3825d
Trevor Dixon <trevordixon@gmail.com>
2013-09-07 15:55:43 -0600

    Encoding with 128 bit key works

--- aes.go
+++ aes.go
@@ -2,13 +2,54 @@ package aes
 
 import ()
 
-type aes struct {
-	key         []byte
-	expandedKey []byte
+type AES struct {
+	key []byte
 }
 
-func New(key []byte) (*aes, error) {
-	a := &aes{key: key}
+func (a *AES) Encode(input []byte) []byte {
+	state := make([]byte, 16)
+	copy(state, input)
 
-	return a, nil
+	numRounds := len(a.key)/2 + 1
+	roundKey := generate128(a.key)
+
+	xor(state, roundKey())
+
+	for i := 0; i < numRounds; i++ {
+		subBytes(state)
+		shiftRows(state)
+		mixColumns(state)
+		xor(state, roundKey())
+	}
+
+	subBytes(state)
+	shiftRows(state)
+	xor(state, roundKey())
+
+	return state
+}
+
+func (a *AES) Decode(input []byte) []byte {
+	return input
+}
+
+func swap(state []byte, i, j int) {
+	temp := state[i]
+	state[i] = state[j]
+	state[j] = temp
+}
+
+func shiftRows(state []byte) {
+	swap(state, 1, 5)
+	swap(state, 5, 9)
+	swap(state, 9, 13)
+
+	swap(state, 6, 10)
+	swap(state, 10, 14)
+	swap(state, 2, 6)
+	swap(state, 6, 10)
+
+	swap(state, 11, 15)
+	swap(state, 7, 11)
+	swap(state, 3, 7)
 }
